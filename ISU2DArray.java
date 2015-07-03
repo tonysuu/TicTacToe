da2008e409ae9ab1406f4c1b09c2a8617ac6c025
tonysuu <tong.su@TOR-TSU-LW7.na.audatex.com>
2015-07-03 15:48:04 -0400

    updated game

--- ISU2DArray.java
+++ ISU2DArray.java
@@ -1,59 +1,64 @@
-import hsa.*;
-
 public class ISU2DArray {
-    public  static final int ROWS = 3;
-    public  static final int COLUMNS = 3;
-    public static int [][] matrix = new int[ROWS][COLUMNS];
+	public  static final int ROWS = 3;
+	public  static final int COLUMNS = 3;
+	public static int [][] matrix = new int[ROWS][COLUMNS];
 
-    
-    private static int checkWinner() {
-        String gameStr = "";
-        for (int row = 0; row < ROWS; row++) {
-            for (int column = 0; column < COLUMNS; column++) {
-                gameStr = gameStr + matrix[row][column];
-            }
-        }
-        
-        for (int column = 0; column < COLUMNS; column++) {
-            for (int row = 0; row < ROWS; row++) {
-                gameStr = gameStr + matrix[row][column];
-            }
-        }
-        
-        for (int i = 0; i < ROWS; i++) {
-            gameStr = gameStr + matrix[i][i];
-        } 
-        
-        for (int i = 0; i < ROWS; i++) {
-            gameStr = gameStr + matrix[ROWS - i - 1][i];
-        }
 
-        
-        for (int i = 0; i < gameStr.length(); i = i + 3) {
-            if (gameStr.substring(i, i + 3).equals("111")) {
-                return(1);
-            }
-        }
-        for (int i = 0; i < gameStr.length(); i = i + 3) {
-            if (gameStr.substring(i, i + 3).equals("222")) {
-                return (2);
-            }
-        }
-            
-        if (gameStr.indexOf("0") != -1) {
-               return (0);
-        }
-        return (-1);
-    }
-    
-    public static void main(String [] args) {
-        for (int row = 0; row < ROWS; row++) {
-            for (int col = 0; col < COLUMNS; col++) {
-                matrix[row][col] = 0;
-            }
-        }
+	private static int checkWinner() {
+		String gameStr = "";
+		for (int row = 0; row < ROWS; row++) {
+			for (int column = 0; column < COLUMNS; column++) {
+				gameStr = gameStr + matrix[row][column];
+			}
+		}
 
-        System.out.println(checkWinner());
-    }
+		for (int column = 0; column < COLUMNS; column++) {
+			for (int row = 0; row < ROWS; row++) {
+				gameStr = gameStr + matrix[row][column];
+			}
+		}
+
+		for (int i = 0; i < ROWS; i++) {
+			gameStr = gameStr + matrix[i][i];
+		} 
+
+		for (int i = 0; i < ROWS; i++) {
+			gameStr = gameStr + matrix[ROWS - i - 1][i];
+		}
+
+
+		for (int i = 0; i < gameStr.length(); i = i + 3) {
+			if (gameStr.substring(i, i + 3).equals("111")) {
+				return(1);
+			}
+		}
+		for (int i = 0; i < gameStr.length(); i = i + 3) {
+			if (gameStr.substring(i, i + 3).equals("222")) {
+				return (2);
+			}
+		}
+
+		if (gameStr.indexOf("0") != -1) {
+			return (0);
+		}
+		return (-1);
+	}
+
+	public static void main(String [] args) {
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLUMNS; col++) {
+				matrix[row][col] = 0;
+			}
+		}
+
+		System.out.println(checkWinner());
+	}
+	public static void clear(){
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLUMNS; col++) {
+				matrix[row][col] = 0;
+			}
+		}
+	}
 
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,57 +1,28 @@
-import hsa.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
 public class TicTacToe {
-    public static final int BOARD_SIDE = 444;
-    public static final int SQUARE_SIDE = 148;
-    public static final int WINDOW_X = 400;
-    public static final int WINDOW_Y = 200;
+	public static final int BOARD_SIDE = 444;
+	public static final int SQUARE_SIDE = 148;
+	public static final int WINDOW_X = 400;
+	public static final int WINDOW_Y = 200;
 
-    static Console c = new Console();
-    
-    public static void drawGrid() {
-	c.setColor(Color.black);
-	c.drawLine(SQUARE_SIDE,0,SQUARE_SIDE,BOARD_SIDE);
-	c.drawLine(SQUARE_SIDE*2,0,SQUARE_SIDE*2,BOARD_SIDE);
-	c.drawLine(0,SQUARE_SIDE,BOARD_SIDE,SQUARE_SIDE);
-	c.drawLine(0,SQUARE_SIDE*2,BOARD_SIDE,SQUARE_SIDE*2);
-    }
-    
-    public static void main(String[] args) {
-	JFrame mainWindow = new JFrame();
-	mainWindow.setTitle("Tic Tac Toe - Computer Science 12 ICS4U ACCI");
-	mainWindow.setSize(BOARD_SIDE, BOARD_SIDE);
-	mainWindow.setLocation(WINDOW_X, WINDOW_Y);
-	mainWindow.setResizable(false);
-	mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	Container board = mainWindow.getContentPane();
-	Color myColor = new Color(0,0,0);
-	ISUBoard isuB = new ISUBoard(myColor, Color.white);
-	board.add(isuB);
-	
-	mainWindow.setContentPane(board);
-	mainWindow.pack();
-	mainWindow.setVisible(true);
-    }
 
-    
-    public static void drawX(int x, int y) {
-	final int HALF_ARM = SQUARE_SIDE / 4;
-	final int ARM_LENGTH = SQUARE_SIDE / 2;
-	c.setColor(Color.white);
-	c.drawLine(x-HALF_ARM,y-HALF_ARM,x-HALF_ARM+ARM_LENGTH,y-HALF_ARM+ARM_LENGTH);
-	c.drawLine(x+HALF_ARM,y-HALF_ARM,x+HALF_ARM-ARM_LENGTH,y-HALF_ARM+ARM_LENGTH);
-    }
-    
-    public static void drawO(int x, int y) {
-	final int CIRCLE_DIAMETER = SQUARE_SIDE / 2;
-	final int CIRCLE_RADIUS = CIRCLE_DIAMETER / 2;
-	c.setColor(Color.white);
-	c.drawOval(x-CIRCLE_RADIUS,y-CIRCLE_RADIUS,CIRCLE_DIAMETER,CIRCLE_DIAMETER);
-    }
-    
-    
+	public static void main(String[] args) {
+		JFrame mainWindow = new JFrame();
+		mainWindow.setTitle("Tic Tac Toe Game");
+		mainWindow.setSize(BOARD_SIDE, BOARD_SIDE);
+		mainWindow.setLocation(WINDOW_X, WINDOW_Y);
+		mainWindow.setResizable(false);
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Color myColor = new Color(0,0,0);
+		ISUBoard isuB = new ISUBoard(myColor, Color.white);
+
+		mainWindow.add(isuB);
+
+		mainWindow.pack();
+		mainWindow.setVisible(true);
+	}
 }
